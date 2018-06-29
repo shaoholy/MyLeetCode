df011b3d9bdc473f415a960cc45a5cfb3cc068a4
shao boran <shao_holy@hotmail.com>
2018-06-29 15:22:08 +0800

    94 NineFour.java
    1, recur
    2, two stacks
    3, one stack

---
+++ Random/94 NineFour.java
@@ -0,0 +1,66 @@
+package Leetcode;
+
+public class NineFour {
+
+}
+
+//1 recursion
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res=new LinkedList<Integer>();
+        getinorder(root, res);
+        return res;
+    }
+    private void getinorder(TreeNode node, List<Integer> res){
+        if(node==null) return;
+        getinorder(node.left, res);
+        res.add(node.val);
+        getinorder(node.right, res);
+        return;  
+    }
+}
+
+//Two Stack
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<Integer> thestack = new Stack<Integer>();
+        Stack<TreeNode> NodeStack= new Stack<TreeNode>();
+        List<Integer> res=new LinkedList<Integer>();
+        if(root==null) return res;
+        TreeNode curr=root;
+        do{
+                if(curr==null){
+                    curr=NodeStack.pop();
+                    res.add(thestack.pop());
+                    }
+                if(curr!=null){
+                    thestack.push(curr.val);
+                    NodeStack.push(curr.right);
+                    curr=curr.left;
+                    }
+            }while(!thestack.empty() && !NodeStack.empty());
+        return res;
+    }
+}
+
+//one stack
+
+public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> list = new ArrayList<Integer>();
+
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    TreeNode cur = root;
+
+    while(cur!=null || !stack.empty()){
+        while(cur!=null){
+            stack.add(cur);
+            cur = cur.left;
+        }
+        cur = stack.pop();
+        list.add(cur.val);
+        cur = cur.right;
+    }
+
+    return list;
+}
