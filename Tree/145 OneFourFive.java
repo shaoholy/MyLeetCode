be28e782a87879b9b182760a7fc7feca8a08e515
shao boran <shao_holy@hotmail.com>
2018-07-01 19:12:54 +0800

    145 updated

--- Tree/145 OneFourFive.java
+++ Tree/145 OneFourFive.java
@@ -1,4 +1,4 @@
-package Leetcode;
+ckage Leetcode;
 
 public class OneFourFive {
 
@@ -42,4 +42,75 @@ class Solution {
     }
 }
 
-//
+//recursion thinking iteration with addFirst
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        Stack<TreeNode> NodeStack=new Stack<TreeNode>();
+        LinkedList<Integer> res=new LinkedList<Integer>();
+        TreeNode cur=root;
+        NodeStack.push(root);
+        while(!NodeStack.empty()) {
+        		cur=NodeStack.pop();
+        		res.addFirst(cur.val);
+        		if(cur.left!=null) NodeStack.push(cur.left);
+        		if(cur.right!=null) NodeStack.push(cur.right);
+        }
+        return res;
+    }
+}
+
+//one stack iteration
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        Stack<TreeNode> NodeStack=new Stack<TreeNode>();
+        LinkedList<Integer> res=new LinkedList<Integer>();
+        TreeNode cur=root;
+        
+        while(cur!=null||!NodeStack.isEmpty()){
+            while(cur!=null) {
+                NodeStack.push(cur);
+                cur=cur.left;
+            }
+            if(NodeStack.peek().right!=null){
+                cur=NodeStack.peek().right;
+            }else{
+                cur=NodeStack.pop();
+                res.add(cur.val);
+                while(!NodeStack.empty() && NodeStack.peek().right==cur){
+                    cur=NodeStack.pop();
+                    res.add(cur.val);
+                }
+                if(NodeStack.empty()) break;
+                cur=NodeStack.peek().right;
+            }
+        }
+        return res;
+    }
+}
+//concise one stack post travesal
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> res=new LinkedList<>();
+    Stack<TreeNode> nodestack=new Stack<>();
+    TreeNode cur=null;
+    expand(nodestack, root);
+    while(!nodestack.empty()) {
+    		if(nodestack.peek().right==null || cur==nodestack.peek().right) {
+    			cur=nodestack.pop();
+                res.add(cur.val);
+    		}else {
+    			expand(nodestack, nodestack.peek().right);
+    		}
+        }
+        return res;
+    }
+    private void expand(Stack<TreeNode> nodestack, TreeNode node){  
+        while(node!=null) {
+                nodestack.push(node);
+                node=node.left;
+        }
+    }
+}
+
