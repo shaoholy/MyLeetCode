ae1cfbbfc1ace97bfe28633b3f32c538179088ac
shao boran <shao_holy@hotmail.com>
2018-07-10 09:28:18 +0800

    4
    O(log(M+N)), zhongdian k/2+k/2 未必等于k。应该用k/2+(k-k/2)

--- Binary search/4 Four.java
+++ Binary search/4 Four.java
@@ -4,13 +4,15 @@ public class Four {
 
 }
 //O(log(M+N)), zhongdian k/2+k/2 未必等于k。应该用k/2+(k-k/2)
+
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if((nums1==null || nums1.length==0) &&(nums2==null || nums2.length==0) ) return 0;
         int m=nums1.length, n=nums2.length;
         int lpoint=(m+n+1)/2,rpoint=(m+n+2)/2;
         return (getkfromtwo(nums1,0,nums2, 0, lpoint)+getkfromtwo(nums1,0,nums2,0,rpoint))/2.0;
         }
-    private int getkfromtwo(int[] nums1, astart, int[] nums2,bstart, k){
+    private int getkfromtwo(int[] nums1, int astart, int[] nums2,int bstart,int k){
         if(astart>nums1.length-1) return nums2[bstart+k-1];
         if(bstart>nums2.length-1) return nums1[astart+k-1];
         if(k==1) return Math.min(nums1[astart], nums2[bstart]);
@@ -19,7 +21,7 @@ class Solution {
         if(astart+k/2-1<nums1.length) mida=nums1[astart+k/2-1];
         if(bstart+k/2-1<nums2.length) midb=nums2[bstart+k/2-1];
         
-        if(mida<midb) return getkfromtwo(nums1, astart+k/2, num2, bstart, k-k/2);
-        else return getkfromtwo(nums1, astart, num2, bstart+k/2, k-k/2);
+        if(mida<midb) return getkfromtwo(nums1, astart+k/2, nums2, bstart, k-k/2);
+        else return getkfromtwo(nums1, astart, nums2, bstart+k/2, k-k/2);
     }
 }
