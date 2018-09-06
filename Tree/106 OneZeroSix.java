2129487201ac6f89b88aa6a08e2be10e305ca744
shao boran <shao_holy@hotmail.com>
2018-09-06 12:39:39 -0700

    0906

--- Tree/106 OneZeroSix.java
+++ Tree/106 OneZeroSix.java
@@ -22,3 +22,39 @@ class Solution {
         return newnode;
     }
 }
+//recur 
+public class Solution {
+    
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        int len = inorder.length; 
+        if(inorder == null || inorder.length == 0){
+            return null; 
+        }
+        
+        
+        return buildTree(inorder, 0, len - 1, postorder, 0, len - 1);
+    }
+    public TreeNode buildTree(int[] inorder, int inStart, int inEnd, int[] postorder, int pStart, int pEnd){
+        
+        int curVal = postorder[pEnd];
+        TreeNode node = new TreeNode(curVal);
+        int inIdx = -1, pIdx = pEnd;
+        for(int i = inStart; i <= inEnd; i++){
+            if(curVal == inorder[i]){
+                inIdx = i;
+                break;
+            }
+        }
+        
+        if(inIdx == inEnd){
+            node.right = null; 
+        }else{
+        //if right exists
+            pIdx = pEnd - (inEnd - inIdx);
+            node.right = buildTree(inorder, inIdx + 1, inEnd, postorder, pIdx, pEnd - 1);
+        }
+        
+        node.left = inIdx == inStart? null : buildTree(inorder, inStart, inIdx - 1, postorder, pStart, pIdx - 1);
+        return node; 
+    }
+}
